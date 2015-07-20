2928a7a4fef8ca9817b5ff3a26beed773ab25418
OldPanda <zjh0930@gmail.com>
2015-07-19 22:24:18 -0700

    solved weird 404 bug(caused by douteng)

--- server.py
+++ server.py
@@ -97,7 +97,7 @@ class Application(tornado.web.Application):
     def __init__(self, db, conf):
         handlers = [
             (r"/", HomeHandler),
-            (r"/auth/douban", DoubanAuthHandler),
+            (r"/auth_douban", DoubanAuthHandler),
             (r"/auth/instagram", InstagramAuthHandler),
             (r"/unlink", UnlinkHandler)
             ]
