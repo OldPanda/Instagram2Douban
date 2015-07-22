b5132e3db39619d1b17d82affd188864da8660da
OldPanda <zjh0930@gmail.com>
2015-07-21 18:01:04 -0700

    reset

--- server.py
+++ server.py
@@ -150,6 +150,14 @@ def del_user(db, user_info):
     """
     inst_id = user_info['user']['id']
     db["users"].remove({"instagram.id": inst_id})
+    '''
+    user = db["users"].find({"instagram.id": inst_id})
+    logging.info("User Douban: [{douban}], Instagram: [{inst}] unlinked. ".format(
+            douban=user["douban"]["uid"],
+            inst=user["instagram"]["username"]
+        )
+    )
+    '''
 
 
 def main():
